9508abcde3a2cb0cc62720a57797d8dc8db0241d
Leonardo Thibes <eu@leonardothibes.com>
2013-12-20 11:19:47 -0200

    implementando parametros default do modulo

---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+class env::params
+{
+	cp_prompt       = true
+	mv_prompt       = true
+	rm_prompt       = true
+	ls_colors       = true
+	bash_completion = true
+	clear_on_logout = true
+	ps1             = '[\u@\h \W]'
+	root_chmod      = 700
+    home_chmod      = 755
+	dirs_in_home    = ['tmp']
+	aliases         = []
+}
